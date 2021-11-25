b50f2574156e84857f0932e9195229bbbeae7e2a
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-11-25 11:56:29 +0300

    model

--- BlogNet/Data/ApplicationDbContext.cs
+++ BlogNet/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace BlogNet.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
