a2d931ee85aba82b8b5318e466633c7f58a46937
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-11-26 11:50:29 +0300

    categories view component

---
+++ BlogNet/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using BlogNet.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogNet.ViewComponents
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriesViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _db.Categories.ToListAsync());
+        }
+    }
+}
