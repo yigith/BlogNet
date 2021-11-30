54e44e4fbb5ae8ccfea34c22268f10001c9021d3
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-11-30 12:27:02 +0300

    manage website, dashboard

---
+++ BlogNet/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogNet.Areas.Admin.Controllers
+{
+    public class DashboardController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BlogNet/Startup.cs
+++ BlogNet/Startup.cs
@@ -63,6 +63,10 @@ namespace BlogNet
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
